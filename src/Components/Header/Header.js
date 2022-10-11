1da0f0e82edcaed8a44a5685fbafeeff46606700
Wd-ruhul <ruhul.wd@gmail.com>
2022-10-11 10:41:55 +0600

    design Navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <nav>
+    <nav className="container-auto bg-indigo-900 text-neutral-200 font-bold p-3 flex gap-x-20 justify-end">
       <Link to="/">Home</Link>
       <Link to="/topics">Topics</Link>
       <Link to="/statistic">Statistic</Link>
       <Link to="/blog">Blog</Link>
-    
     </nav>
   );
 };
